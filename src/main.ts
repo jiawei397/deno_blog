b87d80715306af7165e9c7966e1c850d975c33e1
jw397 <jw397@126.com>
2022-06-09 18:46:43 +0800

    feat: hello world

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { Application } from "https://deno.land/x/oak@v10.5.0/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+app.addEventListener("listen", ({ port }) => {
+  console.log(`Listening on: "http://localhost:${port}`);
+});
+
+await app.listen({ port: 8000 });
