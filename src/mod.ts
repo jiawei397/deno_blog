cb365abc9e46cdd92b2b7cf1a6a895b1ed809785
jw397 <jw397@126.com>
2022-06-11 19:49:59 +0800

    feat(error): 全局异常捕获

--- src/mod.ts
+++ src/mod.ts
@@ -2,11 +2,26 @@ import { Application } from "../deps.ts";
 import { router } from "./routes.ts";
 
 const app = new Application();
+
+app.use(async (context, next) => {
+  try {
+    await next();
+  } catch (error) {
+    context.response.status = error.status || 500;
+    context.response.body = error.message;
+  }
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.addEventListener("listen", ({ port }) => {
   console.log(`Listening on: http://localhost:${port}`);
+});
+
+addEventListener("error", (evt) => {
+  evt.preventDefault();
+  console.error(`global`, evt.error);
 });
 
 await app.listen({ port: 8000 });
