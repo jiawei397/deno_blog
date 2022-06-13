352f5c99dd32e2686b9571a69d7e9e829eb96be8
jw397 <jw397@126.com>
2022-06-13 21:43:46 +0800

    feat(app): version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from "oak_nest";
+import { readYaml } from "./tools/utils.ts";
 
 @Controller("")
 export class AppController {
   @Get("/")
-  version() {
-    return "hello world";
+  async version() {
+    const scriptsConfig = await readYaml<{ version: string }>("scripts.yml");
+    return `<html><h2>${scriptsConfig.version}</h2></html>`;
   }
 }
