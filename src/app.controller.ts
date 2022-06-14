f64360717593628ebf3f641964e6d47768002903
jw397 <jw397@126.com>
2022-06-14 19:32:15 +0800

    feat(app): 打印日志

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from "oak_nest";
+import { Logger } from "./tools/log.ts";
 import { readYaml } from "./tools/utils.ts";
 
 @Controller("")
 export class AppController {
+  constructor(private readonly logger: Logger) {}
+
   @Get("/")
   async version() {
     const scriptsConfig = await readYaml<{ version: string }>("scripts.yml");
+    this.logger.info(`version: ${scriptsConfig.version}`);
     return `<html><h2>${scriptsConfig.version}</h2></html>`;
   }
 }
