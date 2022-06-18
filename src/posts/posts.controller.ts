f8f5ac8119a9502f8ca1d6ee2ccff1db8b2c7a14
jw397 <jw397@126.com>
2022-06-18 14:07:39 +0800

    feat(posts): 创建页面

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from "oak_nest";
+import { Controller, Get, UseGuards } from "oak_nest";
+import { SSOGuard } from "../guards/sso.guard.ts";
 import { Render } from "../tools/ejs.ts";
 
 @Controller("/posts")
@@ -6,5 +7,11 @@ export class PostsController {
   @Get("/")
   async getAll(@Render() render: Render) {
     return await render("posts", {});
+  }
+
+  @Get("/create")
+  @UseGuards(SSOGuard)
+  createPage(@Render() render: Render) {
+    return render("posts/create", {});
   }
 }
