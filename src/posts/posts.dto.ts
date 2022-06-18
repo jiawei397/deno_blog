490b7756ada7cd79c40338f6a49c4d3836f43051
jw397 <jw397@126.com>
2022-06-18 15:30:44 +0800

    feat(posts): 长度限制

--- src/posts/posts.dto.ts
+++ src/posts/posts.dto.ts
@@ -1,21 +1,25 @@
-import { IsOptional, IsString } from "deno_class_validator";
+import { IsOptional, IsString, MaxLength } from "deno_class_validator";
 
 export class CreatePostDto {
   userId: string;
 
   @IsString()
+  @MaxLength(100)
   title: string;
 
   @IsString()
+  @MaxLength(1000)
   content: string;
 }
 
 export class UpdatePostDto {
   @IsString()
   @IsOptional()
+  @MaxLength(100)
   title?: string;
 
   @IsString()
   @IsOptional()
+  @MaxLength(1000)
   content?: string;
 }
