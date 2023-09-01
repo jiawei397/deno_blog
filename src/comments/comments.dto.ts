62abd6502387f6b5d875a4560a82322899499436
jw397 <jw397@126.com>
2023-09-01 18:39:48 +0800

    feat(comments): 校验长度

--- src/comments/comments.dto.ts
+++ src/comments/comments.dto.ts
@@ -1,9 +1,14 @@
-import { IsString } from "deno_class_validator";
+import { IsString, MaxLength, MinLength } from "deno_class_validator";
 
 export class CreateCommentDto {
   @IsString()
+  @MaxLength(1000)
+  @MinLength(10)
   content: string;
 
+  @IsString()
   userId: string;
+
+  @IsString()
   postId: string;
 }
